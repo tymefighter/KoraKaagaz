ff613c3fe71b8c8265b7c2c9e640174180dd23d3
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-10-31 16:36:56 +0530

    Update helpers

--- src/processing/shape/ShapeHelper.java
+++ src/processing/shape/ShapeHelper.java
@@ -17,7 +17,8 @@ public class ShapeHelper {
 	) {
 		int numInitPixels = pixels.size();
 		for(int i = 0;i < numInitPixels;i++) {
-			ArrayList<Pixel> magnifiedPixels = CircleDrawer.drawCircle(
+			
+			ArrayList<Pixel> magnifiedPixels = CircleDrawer.drawCircleFill(
 				pixels.get(i).position, 
 				new Radius(brushRadius.brushRadius), 
 				pixels.get(i).intensity
