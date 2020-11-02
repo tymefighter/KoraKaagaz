83b833f00df344075a8b2feb7b4bc5eeb2331621
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-11-03 01:03:06 +0530

    Begin adding doc for shape helpers

--- src/processing/shape/ShapeHelper.java
+++ src/processing/shape/ShapeHelper.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import processing.utility.*;
 
 /**
+ * Helper Functions for Shape Construction
  *
  * @author Ahmed Zaheer Dadarkar
  * @reviewer Rakesh Kumar
@@ -13,22 +14,55 @@ import processing.utility.*;
 
 public class ShapeHelper {
 	
+	/**
+	 * Method for performing operations to magnify and clean
+	 * up the shape border after the shape border was drawn,
+	 * hence the mention of "post" in the name.
+	 * 
+	 * @param pixels The pixels of the drawn shape
+	 * @param brushRadius Radius of the brush chosen
+	 * @param boardDimension Dimension of the board
+	 * @return the post processed pixels
+	 */
 	protected static ArrayList<Pixel> postDrawProcessing(
 		ArrayList<Pixel> pixels,
 		BrushRadius brushRadius,
 		Dimension boardDimension
 	) {
-		ArrayList<Pixel> magPixels = magnifyPixels(pixels, brushRadius);
-		ArrayList<Pixel> magPixelWithoutDup = removeDuplicates(magPixels);
-		ArrayList<Pixel> finalPixels = removeIllegalPoints(
-			magPixelWithoutDup,
+		// Magnify each pixels to the radius of the brush,
+		// this gives an effect of the shape to be drawing
+		// using a brush of radius given by brushRadius
+		pixels = magnifyPixels(pixels, brushRadius);
+		
+		// Remove any duplicate pixels if present
+		pixels = removeDuplicates(pixels);
+		
+		// Remove any illegal points present, like points
+		// with negative coordinates, or coordinates outside
+		// the board itself
+		pixels = removeIllegalPoints(
+			pixels,
 			boardDimension
 		);
 		
-		return finalPixels;
+		// Return the post processed pixels
+		return pixels;
 	}
 	
-	protected static ArrayList<Pixel> magnifyPixels(
+	protected static ArrayList<Pixel> postFillProcessing(
+		ArrayList<Pixel> pixels,
+		Dimension boardDimension
+	) {
+		pixels = removeDuplicates(pixels);
+		pixels = removeIllegalPoints(
+			pixels,
+			boardDimension
+		);
+		
+		return pixels;
+	}
+	
+	private static ArrayList<Pixel> magnifyPixels(
 		ArrayList<Pixel> pixels,
 		BrushRadius brushRadius
 	) {
@@ -49,14 +83,14 @@ public class ShapeHelper {
 		return updatedPixels;
 	}
 	
-	protected static ArrayList<Pixel> removeDuplicates(
+	private static ArrayList<Pixel> removeDuplicates(
 		ArrayList<Pixel> pixels
 	) {
 		Set<Pixel> pixelSet = new HashSet<Pixel>(pixels);
 		return new ArrayList<Pixel>(pixelSet);
 	}
 	
-	protected static ArrayList<Pixel> removeIllegalPoints(
+	private static ArrayList<Pixel> removeIllegalPoints(
 		ArrayList<Pixel> pixels,
 		Dimension boardDimension
 	) {
