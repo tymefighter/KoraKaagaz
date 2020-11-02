18b8325bd5b8dd48a011e449891145f8b89da102
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-11-03 00:32:27 +0530

    Add devansh circle algorithm

--- src/processing/shape/CircleDrawer.java
+++ src/processing/shape/CircleDrawer.java
@@ -1,6 +1,7 @@
 package processing.shape;
 
 import java.util.ArrayList;
+
 import processing.utility.*;
 
 /**
@@ -9,6 +10,31 @@ import processing.utility.*;
 */
 
 public class CircleDrawer {
+	
+	/** Algorithm for Circle Boundary */
+	public enum AlgorithmCircle {BRESENHAM};
+	
+	/** Algorithm for Circle Fill */
+	public enum AlgorithmFill {DEVANSH};
+	
+	/** Variable storing the algorithm for Circle Boundary */
+	private static AlgorithmCircle circleAlgorithm =
+		AlgorithmCircle.BRESENHAM;
+	
+	/** Variable storing the algorithm for Circle Fill */
+	private static AlgorithmFill fillAlgorithm =
+		AlgorithmFill.DEVANSH;
+	
+	/** Sets the algorithm for Circle Boundary to be used */
+	protected static void setAlgorithmCircle(AlgorithmCircle algorithm) {
+		circleAlgorithm = algorithm;
+	}
+	
+	/** Sets the algorithm for Circle Boundary to be used */
+	protected static void setAlgorithmFill(AlgorithmFill algorithm) {
+		fillAlgorithm = algorithm;
+	}
+	
 	protected static ArrayList<Pixel> drawCircle(
 		Position center,
         Radius radius,
@@ -23,7 +49,34 @@ public class CircleDrawer {
         Radius radius,
         Intensity intensity
 	) {
+		return devanshCircleFill(center, radius, intensity);
+	}
+	
+	private static ArrayList<Pixel> devanshCircleFill(
+		Position center,
+        Radius radius,
+        Intensity intensity
+	) {
+		
 		ArrayList<Pixel> pixels = new ArrayList<Pixel>();
+		int rad = (int)radius.radius;
+		int radSquare = rad * rad;
+		
+		for(int r = center.r - rad;r <= center.r + rad;r++) {
+			for(int c = center.c - rad;c <= center.c + rad;c++) {
+				if(square(r - center.r) + square(c - center.c) <= radSquare) {
+					pixels.add(new Pixel(
+						new Position(r, c),
+						new Intensity(intensity)
+					));
+				}
+			}
+		}
+		
 		return pixels;
+	}
+	
+	private static int square(int x) {
+		return x * x;
 	}
 }
