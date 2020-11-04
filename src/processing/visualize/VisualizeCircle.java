a3799d01c37b84261b0baa05a8baba3c266e181e
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-11-04 11:49:18 +0530

    .

---
+++ src/processing/visualize/VisualizeCircle.java
@@ -0,0 +1,13 @@
+package processing.visualize;
+
+import java.util.ArrayList;
+
+import processing.utility.Dimension;
+import processing.utility.Pixel;
+
+public class VisualizeCircle {
+	public static void main(String[] args) {
+		ArrayList<Pixel> pixels = null;
+		Dimension dimension = new Dimension(1000, 1000);
+	}
+}
